6cea172deccac10b28dd095bd93c09207cc76c7b
Jake Goritski <jgoritski@gmail.com>
2016-12-08 13:53:54 -0500

    add admin urls

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 import views
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-
     url(r'', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
