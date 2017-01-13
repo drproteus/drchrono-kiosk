a08f9e3d6ad84bfa98e6e46e2f9aaf2320f3de18
Jake Goritski <jgoritski@gmail.com>
2017-01-13 11:23:28 -0800

    add kiosk views, templates, and urls

--- kiosk/views.py
+++ kiosk/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from kiosk.forms import SearchForm, ConfigurationForm, DemographicsForm
 
-# Create your views here.
+@login_required
+def home(request):
+    searchForm = SearchForm()
+    kioskMode = request.session.get('kioskMode', False)
+    contextDict = {'form': searchForm, 'kioskMode': kioskMode}
+    return render(request, 'kiosk-index.html', contextDict)
